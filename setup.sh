b4054a2b66952cf1ec0084a3d4033f33f27356d3
kyechou <kuanyenchou@gmail.com>
2019-05-08 20:59:03 -0500

    Not using AUR helpers; add script depends

--- setup.sh
+++ setup.sh
@@ -8,7 +8,11 @@ set -o nounset
 
 cd "$(dirname ${BASH_SOURCE[0]})"
 
-[ $UID -eq 0 ] && (echo '[!] Please run this script without root privilege' >&2; exit 1)
+[ $UID -eq 0 ] && \
+    (echo '[!] Please run this script without root privilege' >&2; exit 1)
+
+# Dependencies needed for this script
+depends=(git curl)
 
 
 get_distro() {
@@ -45,27 +49,52 @@ get_distro() {
     fi
 }
 
-arch_install_yay() {
-    pacman -Q yay &>/dev/null && return 0
-    git clone https://aur.archlinux.org/yay.git
-    pushd yay
-    makepkg -srci --noconfirm
+aur_pkg_exists() {
+    RETURN_CODE="$(curl -I "https://aur.archlinux.org/packages/$1" 2>/dev/null \
+        | head -n1 | cut -d ' ' -f 2)"
+    if [ "$RETURN_CODE" = "200" ]; then
+        return 0    # package found
+    elif [ "$RETURN_CODE" = "404" ]; then
+        return 1    # package not found
+    else
+        echo "[!] AUR HTTP $RETURN_CODE for $1" >&2
+        exit 1
+    fi
+    unset RETURN_CODE
+}
+
+aur_install() {
+    TARGET="$1"
+    shift
+    if pacman -Q "$TARGET" &>/dev/null; then
+        return 0
+    fi
+    if ! aur_pkg_exists "$TARGET"; then
+        echo "[!] AUR package $TARGET not found" >&2
+        return 1
+    fi
+    git clone "https://aur.archlinux.org/$TARGET.git"
+    pushd "$TARGET"
+    makepkg -srci --noconfirm $@
     popd
-    rm -rf yay
+    rm -rf "$TARGET"
+    unset TARGET
 }
 
 main() {
     get_distro
 
     if [ "$DISTRO" = "Arch" ]; then
-        arch_install_yay
-        yay -S --needed --noconfirm spin $@
+        sudo pacman -S --needed --noconfirm --asdeps ${depends[@]}
+        aur_install spin $@
     elif [ "$DISTRO" = "Ubuntu" ]; then
-        sudo apt install spin
+        sudo apt install -y ${depends[@]} spin
     else
         echo "[!] unsupported distribution: $DISTRO" >&2
         exit 1
     fi
+
+    echo '[+] Finished!'
 }
 
 
