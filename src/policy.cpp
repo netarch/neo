8b1e612b5277977609d3e3a410945003c3aca846
kyechou <kuanyenchou@gmail.com>
2019-06-06 07:32:09 +0800

    Move base definition to cpp file

--- src/policy.cpp
+++ src/policy.cpp
@@ -1,1 +1,6 @@
 #include "policy.hpp"
+
+void Policy::load_config(const std::shared_ptr<cpptoml::table>& policy_config
+                         __attribute__((unused)))
+{
+}
--- src/policy.hpp
+++ src/policy.hpp
@@ -12,5 +12,5 @@ public:
     Policy() = default;
     Policy(const Policy&) = default;
 
-    virtual void load_config(const std::shared_ptr<cpptoml::table>&) {}
+    virtual void load_config(const std::shared_ptr<cpptoml::table>&);
 };
