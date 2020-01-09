6ed176f5d139e8ca8e42172eb95e91e2febcffcd
kyechou <kuanyenchou@gmail.com>
2020-01-08 22:04:51 -0600

    Disable optimization compiler flags for now

--- src/plankton.cpp
+++ src/plankton.cpp
@@ -202,7 +202,7 @@ void Plankton::initialize(State *state)
     policy->init(state);
     fwd.init(state, network, policy);
     fwd.enable();
-    //sleep(7);   // DEBUG
+    //sleep(7);   // DEBUG: wait for wireshark to launch
 }
 
 void Plankton::exec_step(State *state)
