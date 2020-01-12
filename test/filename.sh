1b5ee924cb9763fe68a2d56174798af28ae55fb0
kyechou <kuanyenchou@gmail.com>
2020-01-12 15:10:10 -0600

    Fix minor bugs in the testing script

--- test/filename.sh
+++ test/filename.sh
@@ -11,7 +11,7 @@ FILES=$(basename -a $(find ${SRC_DIR} ${THIRD_PARTY_DIR} ${TEST_DIR} -type f) \
 for FILE in $FILES; do
     echo >&2
     echo "[-] multiple source files have the same name '$FILE':" >&2
-    FILEPATHS=$(find ${SRC_DIR} ${TEST_DIR} -type f -name $FILE)
+    FILEPATHS=$(find ${SRC_DIR} ${THIRD_PARTY_DIR} ${TEST_DIR} -type f -name $FILE)
     for FILEPATH in $FILEPATHS; do
         echo "    $FILEPATH" >&2
     done
