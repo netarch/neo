b1b26a391c9ee41d5fbdc3d5019ca7a3ac610b2f
kyechou <kuanyenchou@gmail.com>
2021-01-20 05:57:15 -0600

    Add code coverage config

--- test/all.sh
+++ test/all.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 SCRIPT_DIR="$(dirname $(realpath ${BASH_SOURCE[0]}))"
 
 ${SCRIPT_DIR}/astyle.sh
-${SCRIPT_DIR}/build.sh --debug --tests #--coverage
+${SCRIPT_DIR}/build.sh --debug --tests --coverage
