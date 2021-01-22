757e83baa8461a787b749d747a6540e3dba28be0
kyechou <kuanyenchou@gmail.com>
2021-01-21 19:46:25 -0600

    Run test in all.sh

--- test/all.sh
+++ test/all.sh
@@ -2,6 +2,12 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(dirname $(realpath ${BASH_SOURCE[0]}))"
+PROJECT_DIR="$(realpath ${SCRIPT_DIR}/..)"
+BUILD_DIR="$(realpath ${PROJECT_DIR}/build)"
+export MAKEFLAGS="-j$(nproc)"
 
 ${SCRIPT_DIR}/astyle.sh
 ${SCRIPT_DIR}/build.sh --debug --tests --coverage
+
+cd "${BUILD_DIR}"
+ctest
