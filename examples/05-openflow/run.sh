40e63c58ee93494ada9cd81b3f6f54912adb7ef9
kyechou <kuanyenchou@gmail.com>
2021-01-30 23:21:20 -0600

    Update scripts

---
+++ examples/05-openflow/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname $(realpath ${BASH_SOURCE[0]}))"
+source "${SCRIPT_DIR}/../common.sh"
+
+build
+
+sudo "$NEO" -f -i "$CONF" -o "$RESULTS_DIR"
+sudo chown -R $(id -u):$(id -g) "$RESULTS_DIR"
+cp "$CONF" "$RESULTS_DIR"
