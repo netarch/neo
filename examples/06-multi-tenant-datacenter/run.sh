147363a65b11a76e760b95b89f936eae74aea2aa
kyechou <kuanyenchou@gmail.com>
2021-03-06 12:36:13 -0600

    Add cases for 32, 64 tenants

--- examples/06-multi-tenant-datacenter/run.sh
+++ examples/06-multi-tenant-datacenter/run.sh
@@ -5,7 +5,7 @@ source "${SCRIPT_DIR}/../common.sh"
 
 build
 
-for tenants in 4 8 12; do
+for tenants in 4 8 12 32 64; do
     for updates in 0 $((tenants / 2)) $tenants; do
         ${CONFGEN[*]} -t $tenants -u $updates -s 1 > "$CONF"
         for procs in 1 4 8 16; do
