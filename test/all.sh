90f40661bdcc48cb29b106bb8b412bcd416db5f5
kyechou <kuanyenchou@gmail.com>
2023-03-17 18:54:49 -0500

    Update testing script

--- test/all.sh
+++ test/all.sh
@@ -3,14 +3,14 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
-# PROJECT_DIR="$(realpath "${SCRIPT_DIR}"/..)"
-# BUILD_DIR="$(realpath "${PROJECT_DIR}"/build)"
+PROJECT_DIR="$(realpath "${SCRIPT_DIR}"/..)"
+BUILD_DIR="$(realpath "${PROJECT_DIR}"/build)"
+TEST_DIR="$(realpath "${PROJECT_DIR}"/test)"
 MAKEFLAGS="-j$(nproc)"
 export MAKEFLAGS
 
-"${SCRIPT_DIR}"/style.sh
-"${SCRIPT_DIR}"/build.sh --debug --tests --coverage --gcc
-"${SCRIPT_DIR}"/build.sh --debug --tests --coverage --clang
+"$SCRIPT_DIR"/style.sh
+"$SCRIPT_DIR"/build.sh --debug --tests --coverage --gcc
+"$SCRIPT_DIR"/build.sh --debug --tests --coverage --clang
 
-# cd "${BUILD_DIR}"
-# ctest
+sudo "$BUILD_DIR/test/neotests" --test-data-dir "$TEST_DIR/networks" -d yes
