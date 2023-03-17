24a5405b5644b0dbaa2832b4ea1e55a13484477f
kyechou <kuanyenchou@gmail.com>
2023-03-17 15:24:08 -0500

    Add dockerfile for iptables examples

---
+++ Dockerfiles/iptables/start.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+RULES_FILE="iptables.rules"
+
+cat <<EOF >"$RULES_FILE"
+*filter
+:INPUT DROP [0:0]
+:FORWARD DROP [0:0]
+:OUTPUT ACCEPT [0:0]
+-A INPUT -i eth1 -j ACCEPT
+-A INPUT -i eth2 -j ACCEPT
+-A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+-A FORWARD -i eth1 -j ACCEPT
+-A FORWARD -i eth2 -j ACCEPT
+-A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+COMMIT
+EOF
+
+iptables -F
+iptables -Z
+iptables-restore "$RULES_FILE"
+sleep infinity
