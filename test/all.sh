3f53ecd0c5b12593aec42493d951194f9547e5a8
kyechou <kuanyenchou@gmail.com>
2023-03-19 11:37:48 -0500

    Update gh actions

--- test/all.sh
+++ test/all.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 PROJECT_DIR="$(realpath "${SCRIPT_DIR}"/..)"
 BUILD_DIR="$(realpath "${PROJECT_DIR}"/build)"
-TEST_DIR="$(realpath "${PROJECT_DIR}"/test)"
 MAKEFLAGS="-j$(nproc)"
 export MAKEFLAGS
 
@@ -13,4 +12,5 @@ export MAKEFLAGS
 "$SCRIPT_DIR"/build.sh --debug --tests --coverage --gcc
 "$SCRIPT_DIR"/build.sh --debug --tests --coverage --clang
 
-sudo "$BUILD_DIR/test/neotests" --test-data-dir "$TEST_DIR/networks" -d yes
+cd "$BUILD_DIR"
+ctest --progress --output-on-failure
