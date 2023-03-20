08d4b0094e18207f1d9894d00c4c17e5247dd024
kyechou <kuanyenchou@gmail.com>
2023-03-19 23:00:27 -0500

    Update iptables image to take env as input

--- Dockerfiles/iptables/start.sh
+++ Dockerfiles/iptables/start.sh
@@ -1,29 +1,21 @@
 #!/bin/sh
 
-RULES_FILE="iptables.rules"
+set -eu
 
 sig_handler() {
     kill -KILL -- 0 # all processes in the current process group
     exit 0
 }
 
-cat <<EOF >"$RULES_FILE"
-*filter
-:INPUT DROP [0:0]
-:FORWARD DROP [0:0]
-:OUTPUT ACCEPT [0:0]
--A INPUT -i eth1 -j ACCEPT
--A INPUT -i eth2 -j ACCEPT
--A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
--A FORWARD -i eth1 -j ACCEPT
--A FORWARD -i eth2 -j ACCEPT
--A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-COMMIT
-EOF
+if [ -z "${RULES+x}" ]; then
+    echo '[!] env RULES is unset' >&2
+    exit 1
+fi
 
 iptables -F
 iptables -Z
-iptables-restore "$RULES_FILE"
+# shellcheck disable=SC3001
+iptables-restore <(echo "$RULES")
 trap sig_handler HUP INT QUIT ABRT TERM
 sleep infinity &
 wait
