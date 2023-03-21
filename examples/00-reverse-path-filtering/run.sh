29e3ca74f017d7c122bf2d178c27160cfe6f40e4
kyechou <kuanyenchou@gmail.com>
2023-03-20 19:33:29 -0500

    Update run script - example 00

--- examples/00-reverse-path-filtering/run.sh
+++ examples/00-reverse-path-filtering/run.sh
@@ -6,15 +6,9 @@ source "${SCRIPT_DIR}/../common.sh"
 procs=16
 
 # timeout
-name="output.DOP-$procs.fault"
+name="output.parallel-$procs.fault"
 msg "Verifying $name"
-sudo "$NEO" -fj$procs -i "$CONF" -o "$RESULTS_DIR/$name"
-sudo chown -R "$(id -u):$(id -g)" "$RESULTS_DIR/$name"
-cp "$CONF" "$RESULTS_DIR/$name"
-# dropmon
-name="output.DOP-$procs.fault.dropmon"
-msg "Verifying $name"
-sudo "$NEO" -fdj$procs -i "$CONF" -o "$RESULTS_DIR/$name"
+sudo "$NEO" -f -j $procs -i "$CONF" -o "$RESULTS_DIR/$name"
 sudo chown -R "$(id -u):$(id -g)" "$RESULTS_DIR/$name"
 cp "$CONF" "$RESULTS_DIR/$name"
 
