7753e9dfa2ca7bae2e1851e615a430163a516a11
kyechou <kuanyenchou@gmail.com>
2023-04-10 03:48:47 -0500

    Update example 05

--- examples/05-openflow/run.sh
+++ examples/05-openflow/run.sh
@@ -3,6 +3,11 @@
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 source "${SCRIPT_DIR}/../common.sh"
 
-sudo "$NEO" -f -i "$CONF" -o "$RESULTS_DIR"
-sudo chown -R "$(id -u):$(id -g)" "$RESULTS_DIR"
-cp "$CONF" "$RESULTS_DIR"
+procs=16
+
+for drop in "${DROP_METHODS[@]}"; do
+    name="output.$procs-procs.$drop"
+    run "$name" "$procs" "$drop" "$SCRIPT_DIR/network.toml"
+done
+
+msg "Done"
