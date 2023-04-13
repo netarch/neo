2ba1933f16356483f1626389f94a045b4d62c8b9
kyechou <kuanyenchou@gmail.com>
2023-04-13 13:28:58 -0500

    Add pid_str() for debugging

--- src/process/process.hpp
+++ src/process/process.hpp
@@ -1,10 +1,33 @@
 #pragma once
 
+#include <string>
+
+#include "logger.hpp"
+
 enum pid {
     choose_conn,
     forwarding,
     openflow
 };
+
+static inline std::string pid_str(int n) {
+    switch (n) {
+    case pid::choose_conn: {
+        return "choose_conn";
+    }
+    case pid::forwarding: {
+        return "forwarding";
+    }
+    case pid::openflow: {
+        return "openflow";
+    }
+    default: {
+        logger.error("Invalid process id " + std::to_string(n));
+    }
+    }
+
+    return "";
+}
 
 class Process {
 public:
