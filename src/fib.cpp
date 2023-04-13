4c3f8dd613f6c025152744895857bfb387967b35
kyechou <kuanyenchou@gmail.com>
2023-04-13 13:28:18 -0500

    Update fib naming

--- src/fib.cpp
+++ src/fib.cpp
@@ -9,62 +9,63 @@ FIB_IPNH::FIB_IPNH(Node *l3nh,
                    Interface *l3nh_intf,
                    Node *l2nh,
                    Interface *l2nh_intf)
-    : l3_node(l3nh), l3_intf(l3nh_intf), l2_node(l2nh), l2_intf(l2nh_intf) {}
+    : _l3_node(l3nh), _l3_intf(l3nh_intf), _l2_node(l2nh), _l2_intf(l2nh_intf) {
+}
 
 std::string FIB_IPNH::to_string() const {
     std::string ret;
-    ret = "(" + l3_node->to_string();
-    if (l3_intf) {
-        ret += ", " + l3_intf->to_string();
+    ret = "(" + _l3_node->to_string();
+    if (_l3_intf) {
+        ret += ", " + _l3_intf->to_string();
     }
-    ret += ")-[" + l2_node->to_string();
-    if (l2_intf) {
-        ret += ", " + l2_intf->to_string();
+    ret += ")-[" + _l2_node->to_string();
+    if (_l2_intf) {
+        ret += ", " + _l2_intf->to_string();
     }
     ret += "]";
     return ret;
 }
 
-Node *const &FIB_IPNH::get_l3_node() const {
-    return l3_node;
+Node *const &FIB_IPNH::l3_node() const {
+    return _l3_node;
 }
 
-Interface *const &FIB_IPNH::get_l3_intf() const {
-    return l3_intf;
+Interface *const &FIB_IPNH::l3_intf() const {
+    return _l3_intf;
 }
 
-Node *const &FIB_IPNH::get_l2_node() const {
-    return l2_node;
+Node *const &FIB_IPNH::l2_node() const {
+    return _l2_node;
 }
 
-Interface *const &FIB_IPNH::get_l2_intf() const {
-    return l2_intf;
+Interface *const &FIB_IPNH::l2_intf() const {
+    return _l2_intf;
 }
 
 bool operator<(const FIB_IPNH &a, const FIB_IPNH &b) {
     /**
      * NOTE:
-     * It's important that l3_node is the most significant element, since the
+     * It's important that _l3_node is the most significant element, since the
      * candidates (vector of L3 nodes) are derived from the set of FIB_IPNHs. By
      * making them in order, we eliminate potential duplicate vectors of
      * candidates with different orderings.
      */
-    if (a.l3_node < b.l3_node) {
+    if (a._l3_node < b._l3_node) {
         return true;
-    } else if (a.l3_node > b.l3_node) {
+    } else if (a._l3_node > b._l3_node) {
         return false;
     }
-    if (a.l3_intf < b.l3_intf) {
+    if (a._l3_intf < b._l3_intf) {
         return true;
-    } else if (a.l3_intf > b.l3_intf) {
+    } else if (a._l3_intf > b._l3_intf) {
         return false;
     }
-    if (a.l2_node < b.l2_node) {
+    if (a._l2_node < b._l2_node) {
         return true;
-    } else if (a.l2_node > b.l2_node) {
+    } else if (a._l2_node > b._l2_node) {
         return false;
     }
-    if (a.l2_intf < b.l2_intf) {
+    if (a._l2_intf < b._l2_intf) {
         return true;
     }
     return false;
@@ -75,24 +76,24 @@ bool operator>(const FIB_IPNH &a, const FIB_IPNH &b) {
 }
 
 bool operator==(const FIB_IPNH &a, const FIB_IPNH &b) {
-    return (a.l3_node == b.l3_node) && (a.l3_intf == b.l3_intf) &&
-           (a.l2_node == b.l2_node) && (a.l2_intf == b.l2_intf);
+    return (a._l3_node == b._l3_node) && (a._l3_intf == b._l3_intf) &&
+           (a._l2_node == b._l2_node) && (a._l2_intf == b._l2_intf);
 }
 
 size_t FIB_IPNH_Hash::operator()(const FIB_IPNH &next_hop) const {
     size_t value = 0;
     std::hash<Node *> node_hf;
     std::hash<Interface *> intf_hf;
-    hash::hash_combine(value, node_hf(next_hop.l3_node));
-    hash::hash_combine(value, intf_hf(next_hop.l3_intf));
-    hash::hash_combine(value, node_hf(next_hop.l2_node));
-    hash::hash_combine(value, intf_hf(next_hop.l2_intf));
+    hash::hash_combine(value, node_hf(next_hop._l3_node));
+    hash::hash_combine(value, intf_hf(next_hop._l3_intf));
+    hash::hash_combine(value, node_hf(next_hop._l2_node));
+    hash::hash_combine(value, intf_hf(next_hop._l2_intf));
     return value;
 }
 
 std::string FIB::to_string() const {
     std::string ret = "FIB:\n";
-    for (const auto &entry : tbl) {
+    for (const auto &entry : _fib) {
         ret += entry.first->to_string() + " -> [";
         for (auto it = entry.second.begin(); it != entry.second.end(); ++it) {
             ret += " " + it->to_string();
@@ -103,22 +104,22 @@ std::string FIB::to_string() const {
 }
 
 void FIB::set_ipnhs(Node *node, std::set<FIB_IPNH> &&next_hops) {
-    tbl[node] = next_hops;
+    _fib[node] = next_hops;
 }
 
 void FIB::add_ipnh(Node *node, FIB_IPNH &&next_hop) {
-    tbl[node].insert(next_hop);
+    _fib[node].insert(next_hop);
 }
 
 const std::set<FIB_IPNH> &FIB::lookup(Node *const node) const {
-    return tbl.at(node);
+    return _fib.at(node);
 }
 
 size_t FIBHash::operator()(const FIB *const &fib) const {
     size_t value = 0;
     std::hash<Node *> node_hf;
     FIB_IPNH_Hash ipnh_hf;
-    for (const auto &entry : fib->tbl) {
+    for (const auto &entry : fib->_fib) {
         hash::hash_combine(value, node_hf(entry.first));
         for (const auto &nh : entry.second) {
             hash::hash_combine(value, ipnh_hf(nh));
@@ -128,5 +129,5 @@ size_t FIBHash::operator()(const FIB *const &fib) const {
 }
 
 bool FIBEq::operator()(const FIB *const &a, const FIB *const &b) const {
-    return a->tbl == b->tbl;
+    return a->_fib == b->_fib;
 }
--- src/fib.hpp
+++ src/fib.hpp
@@ -10,14 +10,14 @@ class Interface;
 class FIB_IPNH // FIB entry for an IP next hop
 {
 private:
-    Node *l3_node;      // L3 next hop node
-    Interface *l3_intf; // L3 next hop interface
-    Node *l2_node;      // L2 next hop node
-    Interface *l2_intf; // L2 next hop interface
-    /*
-     * Note that l3_node and l2_node should/would never be nullptr, but l3_intf
-     * and l2_intf can both be nullptrs (at the same time), which only occurs
-     * when l3_node and l2_node are both the same.
+    Node *_l3_node;      // L3 next hop node
+    Interface *_l3_intf; // L3 next hop interface
+    Node *_l2_node;      // L2 next hop node
+    Interface *_l2_intf; // L2 next hop interface
+    /**
+     * Note that _l3_node and _l2_node should/would never be nullptr, but
+     * _l3_intf and _l2_intf can both be nullptrs (at the same time), which only
+     * occurs when _l3_node and _l2_node are both the same.
      */
 
     friend class FIB_IPNH_Hash;
@@ -36,10 +36,10 @@ public:
     FIB_IPNH &operator=(FIB_IPNH &&) = default;
 
     std::string to_string() const;
-    Node *const &get_l3_node() const;
-    Interface *const &get_l3_intf() const;
-    Node *const &get_l2_node() const;
-    Interface *const &get_l2_intf() const;
+    Node *const &l3_node() const;
+    Interface *const &l3_intf() const;
+    Node *const &l2_node() const;
+    Interface *const &l2_intf() const;
 };
 
 bool operator<(const FIB_IPNH &, const FIB_IPNH &);
@@ -51,12 +51,12 @@ public:
     size_t operator()(const FIB_IPNH &) const;
 };
 
-/*
+/**
  * An FIB holds the dataplane information for the current EC.
  */
 class FIB {
 private:
-    std::map<Node *, std::set<FIB_IPNH>> tbl;
+    std::map<Node *, std::set<FIB_IPNH>> _fib;
 
     friend class FIBHash;
     friend class FIBEq;
