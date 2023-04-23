5797fd511a87f8a9dd4ed155b03ccf5a6132125f
kyechou <kuanyenchou@gmail.com>
2023-04-23 16:19:47 -0500

    Update 06 run script

--- examples/06-multi-tenant-datacenter/run.sh
+++ examples/06-multi-tenant-datacenter/run.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 source "${SCRIPT_DIR}/../common.sh"
 
-for tenants in 4 8 12 32 64; do
+for tenants in 4 8 16 32 64; do
     for updates in 0 $((tenants / 2)) $tenants; do
         "${CONFGEN[@]}" -t "$tenants" -u "$updates" -s 1 > "$CONF"
         for procs in 1 4 8 16 20; do
