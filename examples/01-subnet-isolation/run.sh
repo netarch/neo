94a634ae9a9b0b3fcf913e0fd238d137e5fff5b9
kyechou <kuanyenchou@gmail.com>
2023-04-23 18:32:26 -0500

    Update experiment scripts

--- examples/01-subnet-isolation/run.sh
+++ examples/01-subnet-isolation/run.sh
@@ -3,10 +3,10 @@
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 source "${SCRIPT_DIR}/../common.sh"
 
-for subnets in 4 8 10 12; do
+for subnets in 4 8 12; do
     hosts=$subnets
     "${CONFGEN[@]}" --subnets $subnets --hosts $hosts > "$CONF"
-    for procs in 1 4 8 16 20; do
+    for procs in 1 4 8 16; do
         for drop in "${DROP_METHODS[@]}"; do
             name="output.$subnets-subnets.$hosts-hosts.$procs-procs.$drop"
             run "$name" "$procs" "$drop" "$CONF"
@@ -16,10 +16,10 @@ for subnets in 4 8 10 12; do
 done
 
 # faulty configuration
-for subnets in 4 8 10 12; do
+for subnets in 4 8 12; do
     hosts=$subnets
     "${CONFGEN[@]}" --subnets $subnets --hosts $hosts --fault > "$CONF"
-    for procs in 1 4 8 16 20; do
+    for procs in 1 4 8 16; do
         for drop in "${DROP_METHODS[@]}"; do
             name="output.$subnets-subnets.$hosts-hosts.$procs-procs.$drop.fault"
             run "$name" "$procs" "$drop" "$CONF"
@@ -27,18 +27,5 @@ for subnets in 4 8 10 12; do
     done
     rm "$CONF"
 done
-
-# msg "Populating measurements..."
-# extract_stats "$RESULTS_DIR" > "$RESULTS_DIR/stats.csv"
-# extract_latency "$RESULTS_DIR" > "$RESULTS_DIR/latency.csv"
-
-# msg "Collecting false violations..."
-# > "$RESULTS_DIR/false_violations.txt"
-# for dir in "$RESULTS_DIR"/*; do
-#     [[ ! -d "$dir" || "$dir" == *"fault"* ]] && continue
-#     if grep violated "$dir/main.log" >/dev/null; then
-#         echo "$(basename $dir)" >> "$RESULTS_DIR/false_violations.txt"
-#     fi
-# done
 
 msg "Done"
--- examples/03-load-balancing/run.sh
+++ examples/03-load-balancing/run.sh
@@ -5,7 +5,7 @@ source "${SCRIPT_DIR}/../common.sh"
 
 for lbs in 2 4; do
     srvs=$lbs
-    for algo in rr sh dh; do    # round-robin, source-hashing, destination-hashing
+    for algo in rr sh dh; do # round-robin, source-hashing, destination-hashing
         "${CONFGEN[@]}" -l $lbs -s $srvs -a $algo > "$CONF"
         for procs in 1 4 8 16; do
             for drop in "${DROP_METHODS[@]}"; do
@@ -16,62 +16,5 @@ for lbs in 2 4; do
         rm "$CONF"
     done
 done
-
-# msg "Populating measurements..."
-
-# extract_stats() {
-#     results_dir="$1"
-#     [[ ! -d "$results_dir" ]] && die "Invalid directory: $results_dir"
-
-#     echo 'algorithm, fault, dropmon, parallel processes, nodes, links, Policy, Connection ECs, Time (microseconds), Memory (kilobytes)'
-#     for dir in "$results_dir"/*; do
-#         [[ ! -d "$dir" ]] && continue
-#         name="$(basename "$dir")"
-#         oldIFS=$IFS
-#         IFS="."
-#         for parameter in $name; do
-#             if [[ "$parameter" == "algo-"* ]]; then
-#                 algorithm=$(echo $parameter | sed 's/algo-//')
-#             fi
-#         done
-#         IFS=$oldIFS
-#         echo "$algorithm, $(extract_per_session_stats $dir)"
-#     done
-# }
-# extract_stats "$RESULTS_DIR" > "$RESULTS_DIR/stats.csv"
-
-# extract_latency() {
-#     results_dir="$1"
-#     [[ ! -d "$results_dir" ]] && die "Invalid directory: $results_dir"
-
-#     echo -n 'algorithm, fault, dropmon, parallel processes, nodes, links, Policy, Connection ECs, '
-#     echo -n 'Overall latency t1 (nsec), Overall latency (nsec), '
-#     echo -n 'Rewind latency t1 (nsec), Rewind latency (nsec), Rewind injection count, '
-#     echo -n 'Packet latency t1 (nsec), Packet latency (nsec), Timeout (nsec), '
-#     echo    'Kernel drop latency (nsec)'
-#     for dir in "$results_dir"/*; do
-#         [[ ! -d "$dir" ]] && continue
-#         name="$(basename "$dir")"
-#         oldIFS=$IFS
-#         IFS="."
-#         for parameter in $name; do
-#             if [[ "$parameter" == "algo-"* ]]; then
-#                 algorithm=$(echo $parameter | sed 's/algo-//')
-#             fi
-#         done
-#         IFS=$oldIFS
-#         echo "$algorithm, $(extract_per_session_latency $dir)"
-#     done
-# }
-# extract_latency "$RESULTS_DIR" > "$RESULTS_DIR/latency.csv"
-
-# msg "Collecting false violations..."
-# > "$RESULTS_DIR/false_violations.txt"
-# for dir in "$RESULTS_DIR"/*; do
-#     [[ ! -d "$dir" || "$dir" == *"algo-dh"* ]] && continue
-#     if grep violated "$dir/main.log" >/dev/null; then
-#         echo "$(basename $dir)" >> "$RESULTS_DIR/false_violations.txt"
-#     fi
-# done
 
 msg "Done"
