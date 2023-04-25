14524c2bf05a9732d6477070225347088e93ff55
kyechou <kuanyenchou@gmail.com>
2023-04-25 08:38:48 -0500

    Plot 03 stats and latency

--- examples/plot.py
+++ examples/plot.py
@@ -78,7 +78,7 @@ def plot_02_perf_vs_apps_hosts(df, outDir):
 
         ax.grid(axis='y')
         # ax.set_yscale('log')
-        ax.set_xlabel('Applications and hosts', fontsize=22)
+        ax.set_xlabel('Applications and hosts/app', fontsize=22)
         ax.set_ylabel('Time (seconds)', fontsize=22)
         ax.tick_params(axis='both', which='both', labelsize=22)
         ax.tick_params(axis='x', which='both', top=False, bottom=False)
@@ -193,7 +193,7 @@ def plot_02_perf_vs_drop_methods(df, outDir):
                   fancybox=False)
         ax.grid(axis='y')
         # ax.set_yscale('log')
-        ax.set_xlabel('Applications and hosts', fontsize=22)
+        ax.set_xlabel('Applications and hosts/app', fontsize=22)
         ax.set_ylabel('Time (seconds)', fontsize=22)
         ax.tick_params(axis='both', which='both', labelsize=22)
         ax.tick_params(axis='x', which='both', top=False, bottom=False)
@@ -228,6 +228,100 @@ def plot_02_stats(invDir, outDir):
     plot_02_perf_vs_apps_hosts(df, outDir)
     plot_02_perf_vs_nprocs(df, outDir)
     plot_02_perf_vs_drop_methods(df, outDir)
+
+
+def plot_03_stats(invDir, outDir):
+    df = pd.read_csv(os.path.join(invDir, 'stats.csv'))
+    # Filter rows
+    df = df[df.procs == 1]
+    df = df[df.drop_method == 'timeout']
+    df = df[df.invariant == 1]
+    # Filter columns
+    df = df.drop([
+        'servers', 'procs', 'drop_method', 'num_nodes', 'num_links',
+        'num_updates', 'total_conn', 'invariant', 'independent_cec', 'violated'
+    ],
+                 axis=1)
+    # Sorting
+    df = df.sort_values(by=['lbs', 'algorithm'])
+    # Change units
+    df['inv_time'] /= 1e6  # usec -> sec
+    df['inv_memory'] /= 1024  # KiB -> MiB
+
+    time_df = df.pivot(index='lbs', columns='algorithm',
+                       values='inv_time').reset_index()
+    time_df = time_df.rename(
+        columns={
+            'rr': 'Time (round-robin)',
+            'dh': 'Time (dest-hashing)',
+            'sh': 'Time (source-hashing)',
+        })
+    mem_df = df.pivot(index='lbs', columns='algorithm',
+                      values='inv_memory').reset_index()
+    mem_df = mem_df.rename(
+        columns={
+            'rr': 'Memory (round-robin)',
+            'dh': 'Memory (dest-hashing)',
+            'sh': 'Memory (source-hashing)',
+        })
+    merged_df = pd.merge(time_df, mem_df, on=['lbs'])
+
+    # Plot time/memory
+    ax = merged_df.plot(
+        x='lbs',
+        y=[
+            'Time (round-robin)', 'Time (dest-hashing)',
+            'Time (source-hashing)'
+        ],
+        kind='bar',
+        legend=False,
+        width=0.8,
+        xlabel='',
+        ylabel='',
+        rot=0,
+    )
+    ax = merged_df.plot(
+        x='lbs',
+        secondary_y=[
+            'Memory (round-robin)', 'Memory (dest-hashing)',
+            'Memory (source-hashing)'
+        ],
+        mark_right=False,
+        kind='bar',
+        legend=False,
+        width=0.8,
+        xlabel='',
+        ylabel='',
+        rot=0,
+    )
+
+    # Merge legends
+    h1, l1 = ax.get_legend_handles_labels()
+    h2, l2 = ax.right_ax.get_legend_handles_labels()
+    ax.legend(h1 + h2,
+              l1 + l2,
+              bbox_to_anchor=(1.12, 1.28),
+              columnspacing=0.8,
+              ncol=2,
+              fontsize=14,
+              frameon=False,
+              fancybox=False)
+
+    ax.grid(axis='y')
+    # ax.set_yscale('log')
+    ax.set_xlabel('LBs and servers/LB', fontsize=22)
+    ax.set_ylabel('Time (seconds)', fontsize=22)
+    ax.tick_params(axis='both', which='both', labelsize=22)
+    ax.tick_params(axis='x', which='both', top=False, bottom=False)
+    ax.right_ax.grid(axis='y')
+    mem_start_val = (floor(df.inv_memory.min()) - 1) // 10 * 10
+    ax.right_ax.set_ylim(bottom=mem_start_val)
+    ax.right_ax.set_ylabel('Memory (MiB)', fontsize=22)
+    ax.right_ax.tick_params(axis='both', which='both', labelsize=18)
+    fig = ax.get_figure()
+    fn = os.path.join(outDir, ('03.perf-lbs.pdf'))
+    fig.savefig(fn, bbox_inches='tight')
+    plt.close('all')
 
 
 def plot_06_perf_vs_tenants(df, outDir):
@@ -692,7 +786,7 @@ def plot_02_latency(invDir, outDir):
     df = df[df.procs == 1]
     # Filter columns
     df = df.drop([
-        'overall_lat', 'rewind_injections', 'apps', 'hosts', 'procs', 'fault',
+        'overall_lat', 'rewind_injections', 'hosts', 'procs', 'fault',
         'num_nodes', 'num_links', 'num_updates', 'total_conn', 'invariant',
         'independent_cec', 'violated'
     ],
@@ -700,14 +794,170 @@ def plot_02_latency(invDir, outDir):
     df = df.reset_index().drop(['index'], axis=1)
 
     # latency line charts
-    plot_latency(df[df.pkt_lat > 0],
-                 os.path.join(outDir, '02.latency.recv.pdf'),
-                 sample_limit=300)
+    recv_df = df[df.pkt_lat > 0]
+    for apps in recv_df.apps.unique():
+        plot_latency(
+            recv_df[recv_df.apps == apps],
+            os.path.join(outDir,
+                         ('02.latency.' + str(apps) + '-apps.recv.pdf')),
+            sample_limit=None)
     plot_latency(df[df.drop_lat > 0],
                  os.path.join(outDir, '02.latency.drop.pdf'))
 
     # latency CDF
     plot_latency_cdf(df, outDir, os.path.basename(invDir)[:2])
+
+
+def plot_03_latency(invDir, outDir):
+    df = pd.read_csv(os.path.join(invDir, 'lat.csv'))
+    # Merge latency values
+    df.loc[:, 'latency'] = df['pkt_lat'] + df['drop_lat']
+    # Filter rows
+    df = df[df.procs == 1]
+    df = df[df.drop_method == 'timeout']
+    df = df[df.invariant == 1]
+    # Filter columns
+    df = df.drop([
+        'overall_lat', 'servers', 'algorithm', 'procs', 'drop_method',
+        'num_nodes', 'num_links', 'num_updates', 'total_conn', 'invariant',
+        'independent_cec', 'violated'
+    ],
+                 axis=1)
+    df = df.reset_index().drop(['index'], axis=1)
+
+    def _plot_latency(df, outFn):
+        # Filter columns
+        df = df.drop(
+            ['rewind_lat', 'rewind_injections', 'pkt_lat', 'drop_lat', 'lbs'],
+            axis=1)
+        # Rename column titles
+        df = df.rename(columns={
+            'latency': 'Latency',
+            'timeout': 'Drop timeout'
+        })
+
+        ax = df.plot(
+            y=['Latency', 'Drop timeout'],
+            kind='line',
+            legend=False,
+            xlabel='',
+            ylabel='',
+            rot=0,
+            lw=2,
+        )
+        ax.legend(bbox_to_anchor=(1.0, 1.2),
+                  columnspacing=0.8,
+                  ncol=2,
+                  fontsize=22,
+                  frameon=False,
+                  fancybox=False)
+        ax.grid(axis='y')
+        # ax.set_yscale('log')
+        ax.set_xlabel('Packet injections', fontsize=22)
+        ax.set_ylabel('Latency (microseconds)', fontsize=22)
+        ax.tick_params(axis='both', which='both', labelsize=22)
+        ax.tick_params(axis='x',
+                       which='both',
+                       top=False,
+                       bottom=False,
+                       labelbottom=False)
+        fig = ax.get_figure()
+        fig.savefig(outFn, bbox_inches='tight')
+        plt.close(fig)
+
+    def _plot_latency_cdf(df, outFn):
+        df.loc[:, 'latency_w_reset'] = df['latency'] + df['rewind_lat']
+        # Filter columns
+        df = df.drop([
+            'rewind_lat', 'rewind_injections', 'pkt_lat', 'drop_lat',
+            'timeout', 'lbs'
+        ],
+                     axis=1)
+        # Rename column titles
+        df = df.rename(columns={
+            'latency': 'Exclude reset',
+            'latency_w_reset': 'Include reset'
+        })
+
+        def __get_cdf_df(df, col):
+            df = df[col].to_frame().dropna()
+            df = df.sort_values(by=[col]).reset_index().rename(
+                columns={col: 'latency'})
+            df[col] = df.index + 1
+            df = df[df.columns.drop(list(df.filter(regex='index')))]
+            return df
+
+        cdf_df = pd.DataFrame()
+        peak_latencies = []
+
+        for col in list(df.columns):
+            df1 = __get_cdf_df(df, col)
+            peak_latencies.append(df1.latency.iloc[-1])
+            if cdf_df.empty:
+                cdf_df = df1
+            else:
+                cdf_df = pd.merge(cdf_df, df1, how='outer', on='latency')
+            del df1
+
+        df = cdf_df.sort_values(by='latency').interpolate(limit_area='inside')
+        del cdf_df
+
+        # Change units
+        df['latency'] /= 1e6  # usec -> sec
+        peak_latencies = [l / 1e6 for l in peak_latencies]
+
+        ax = df.plot(
+            x='latency',
+            y=['Exclude reset', 'Include reset'],
+            kind='line',
+            legend=False,
+            xlabel='',
+            ylabel='',
+            rot=0,
+            lw=LINE_WIDTH,
+        )
+        ax.legend(bbox_to_anchor=(1.07, 1.2),
+                  columnspacing=0.8,
+                  ncol=2,
+                  fontsize=22,
+                  frameon=False,
+                  fancybox=False)
+        ax.grid(axis='both')
+        ax.set_xscale('log')
+        ax.set_xlabel('Latency (seconds)', fontsize=24)
+        ax.set_ylabel('Packet injections', fontsize=24)
+        ax.tick_params(axis='both', which='both', labelsize=24)
+        ax.tick_params(axis='x', which='both', top=False, bottom=False)
+        i = 0
+        for peak_lat in peak_latencies:
+            ax.axvline(peak_lat,
+                       ymax=0.95,
+                       linestyle='--',
+                       linewidth=LINE_WIDTH - 1,
+                       color=colors[i])
+            i += 1
+        fig = ax.get_figure()
+        fig.savefig(outFn, bbox_inches='tight')
+        plt.close(fig)
+
+    tbl = {}
+
+    for lbs in df.lbs.unique():
+        lbs_df = df[df.lbs == lbs]
+        injections = lbs_df.rewind_injections
+        tbl[lbs] = {'lbs': lbs}
+        tbl[lbs]['num_reset'] = injections[injections != -1].count()
+        tbl[lbs]['num_reset_injections'] = injections[injections != -1].sum()
+
+        # latency line charts
+        _plot_latency(
+            lbs_df,
+            os.path.join(outDir, ('03.latency.' + str(lbs) + '-lbs.pdf')))
+
+    # latency CDF
+    _plot_latency_cdf(df, os.path.join(outDir, '03.latency-cdf.pdf'))
+
+    print(pd.DataFrame(tbl))
 
 
 def plot_06_latency(invDir, outDir):
@@ -768,7 +1018,8 @@ def main():
         plot_02_stats(targetDir, outDir)
         plot_02_latency(targetDir, outDir)
     elif exp_id == '03':
-        pass
+        plot_03_stats(targetDir, outDir)
+        plot_03_latency(targetDir, outDir)
     elif exp_id == '06':
         plot_06_stats(targetDir, outDir)
         plot_06_latency(targetDir, outDir)
