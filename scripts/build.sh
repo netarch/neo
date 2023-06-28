1b4bc07e8ba084eb7badd3936dc4fb5d084ace79
kyechou <kuanyenchou@gmail.com>
2023-06-28 02:25:19 -0500

    Make build script accept CLI options

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,8 +6,50 @@ SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
 BUILD_DIR="${PROJECT_DIR}/build"
 
+die() {
+    echo -e "[!] ${1-}" >&2
+    exit 1
+}
+
 [ $UID -eq 0 ] && die 'Please run this script without root privilege'
 
-cmake --build "$BUILD_DIR" -j "$(nproc)"
+usage() {
+    cat <<EOF
+[!] Usage: $(basename "${BASH_SOURCE[0]}") [options]
+
+    Options:
+    -h, --help          Print this message and exit
+    -j, --parallel N    Number of parallel build tasks
+EOF
+}
+
+parse_args() {
+    NUM_TASKS=$(nproc)
+
+    while :; do
+        case "${1-}" in
+        -h | --help)
+            usage
+            exit
+            ;;
+        -j | --parallel)
+            NUM_TASKS="${2-}"
+            shift
+            ;;
+        -?*) die "Unknown option: $1\n$(usage)" ;;
+        *) break ;;
+        esac
+        shift
+    done
+}
+
+main() {
+    # Parse script arguments
+    parse_args "$@"
+    # Build
+    cmake --build "$BUILD_DIR" -j "$NUM_TASKS"
+}
+
+main "$@"
 
 # vim: set ts=4 sw=4 et:
