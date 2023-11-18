31a65c5de93f20b3cec61550991c7daaeba836b3
kyechou <kuanyenchou@gmail.com>
2023-11-17 19:47:22 -0600

    Extend reply-reachability for NAT nodes

--- src/invariant/reply-reachability.cpp
+++ src/invariant/reply-reachability.cpp
@@ -23,6 +23,21 @@ void ReplyReachability::init() {
     model.set_violated(false);
 }
 
+static inline bool request_is_accepted(int mode, int proto_state) {
+    return (mode == fwd_mode::ACCEPTED && PS_IS_REQUEST(proto_state)) ||
+           (mode == fwd_mode::FIRST_FORWARD && PS_REQ_ACK_OR_REP(proto_state));
+}
+
+static inline bool other_executable_conns_exist(const Model &model) {
+    int current_conn = model.get_conn();
+    for (int conn = 0; conn < model.get_num_conns(); ++conn) {
+        if (conn != current_conn && model.get_executable_for_conn(conn) > 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int ReplyReachability::check_violation() {
     int mode = model.get_fwd_mode();
     int proto_state = model.get_proto_state();
@@ -31,31 +46,44 @@ int ReplyReachability::check_violation() {
         (PS_IS_UDP(proto_state) && proto_state < PS_UDP_REP) ||
         (PS_IS_ICMP_ECHO(proto_state) && proto_state < PS_ICMP_ECHO_REP)) {
         // request
-        Node *rx_node = model.get_rx_node();
-        if (((mode == fwd_mode::ACCEPTED || (mode == fwd_mode::FIRST_FORWARD &&
-                                             PS_REQ_ACK_OR_REP(proto_state))) &&
-             target_nodes.count(rx_node) == 0) ||
-            mode == fwd_mode::DROPPED) {
-            // if the request is accepted by a wrong node or dropped
+        if (((request_is_accepted(mode, proto_state) &&
+              target_nodes.count(model.get_rx_node()) == 0) ||
+             mode == fwd_mode::DROPPED) &&
+            !other_executable_conns_exist(model)) {
+            // The request (or session construction packets) is accepted by a
+            // wrong node or dropped, while no other executable connections
+            // exist.
             model.set_violated(reachable);
             model.set_choice_count(0);
         } else {
-            // If the request (or session construction packets) hasn't been
-            // accepted or dropped, there is nothing to check.
+            // The request (or session construction packets) is either accepted
+            // by the correct nodes, hasn't been accepted or dropped, or there
+            // are other executable connections remainging. If there are other
+            // executable connections, then those connections should be related
+            // to the current connection (e.g., after an NAT). In either case,
+            // we continue the exploration until otherwise.
             return POL_NULL;
         }
     } else if (PS_IS_REPLY(proto_state)) {
         // reply
         bool reached;
-        if (mode == fwd_mode::ACCEPTED) {
-            reached = (model.get_pkt_location() == model.get_tx_node());
-        } else if (mode == fwd_mode::DROPPED) {
+        if (mode == fwd_mode::ACCEPTED &&
+            model.get_pkt_location() == model.get_tx_node()) {
+            // The reply packet is accepted by the original sender.
+            reached = true;
+        } else if (((mode == fwd_mode::ACCEPTED &&
+                     model.get_pkt_location() != model.get_tx_node()) ||
+                    mode == fwd_mode::DROPPED) &&
+                   !other_executable_conns_exist(model)) {
+            // The reply packet is accepted by an incorrect sender or is
+            // dropped, while none of the other connections are executable.
             reached = false;
         } else {
-            // If the reply hasn't been accepted or dropped, there is nothing to
-            // check.
+            // Either the reply packet hasn't been accepted or dropped, or there
+            // are other executable connections remaining.
             return POL_NULL;
         }
+
         model.set_violated(reachable != reached);
         model.set_choice_count(0);
     }
