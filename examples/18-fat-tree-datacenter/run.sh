4fa97a5b56b57260ccd0bff6a775d0105e8147aa
kyechou <kuanyenchou@gmail.com>
2023-11-28 10:28:30 -0600

    Update run script

--- examples/18-fat-tree-datacenter/run.sh
+++ examples/18-fat-tree-datacenter/run.sh
@@ -3,15 +3,43 @@
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 source "${SCRIPT_DIR}/../common.sh"
 
+# for arity in 4 6 8 10 12 14 16; do
+#     for update_pct in 0 50 100; do
+#         "${CONFGEN[@]}" -k "$arity" -u "$update_pct" -s 1 >"$CONF"
+#         for procs in 1 4 8 12 16; do
+#             for drop in "${DROP_METHODS[@]}"; do
+#                 name="output.$arity-ary.$update_pct-update-pct.$procs-procs.$drop"
+#                 run "$name" "$procs" "$drop" "$CONF"
+#             done
+#         done
+#         rm "$CONF"
+#     done
+# done
+
 for arity in 4 6 8 10 12 14 16; do
-    "${CONFGEN[@]}" -k "$arity" -s 1 >"$CONF"
-    for procs in 1 4 8 12 16; do
-        for drop in "${DROP_METHODS[@]}"; do
-            name="output.$arity-ary.$procs-procs.$drop"
-            run "$name" "$procs" "$drop" "$CONF"
+    for update_pct in 0 50 100; do
+        "${CONFGEN[@]}" -k "$arity" -u "$update_pct" -s 1 >"$CONF"
+        for procs in 1 4 8 12 16; do
+            for drop in "timeout"; do
+                name="output.$arity-ary.$update_pct-update-pct.$procs-procs.$drop"
+                run "$name" "$procs" "$drop" "$CONF"
+            done
         done
+        rm "$CONF"
     done
-    rm "$CONF"
+done
+
+for arity in 4 6 8 10 12 14 16; do
+    for update_pct in 0 50 100; do
+        "${CONFGEN[@]}" -k "$arity" -u "$update_pct" -s 1 >"$CONF"
+        for procs in 1 4 8 12 16; do
+            for drop in "dropmon" "ebpf"; do
+                name="output.$arity-ary.$update_pct-update-pct.$procs-procs.$drop"
+                run "$name" "$procs" "$drop" "$CONF"
+            done
+        done
+        rm "$CONF"
+    done
 done
 
 msg "Done"
