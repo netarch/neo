bd1e2cc8f311cc53b8906fe7ddd4faabbb67d539
kyechou <kuanyenchou@gmail.com>
2023-12-01 08:57:02 -0600

    Update example 18 stats

--- examples/18-fat-tree-datacenter/run.sh
+++ examples/18-fat-tree-datacenter/run.sh
@@ -7,9 +7,10 @@ for arity in 4 6 8 10 12; do
     for update_pct in 0 50 100; do
         "${CONFGEN[@]}" -k "$arity" -u "$update_pct" -s 1 >"$CONF"
         for procs in 1 4 8 12 16; do
-            drop="timeout"
-            name="output.$arity-ary.$update_pct-update-pct.$procs-procs.$drop"
-            run "$name" "$procs" "$drop" "$CONF"
+            for drop in "${DROP_METHODS[@]}"; do
+                name="output.$arity-ary.$update_pct-update-pct.$procs-procs.$drop"
+                run "$name" "$procs" "$drop" "$CONF"
+            done
         done
         rm "$CONF"
     done
