e0f889dc8afabdeb77c622a3cce023f5583e4e26
kyechou <kuanyenchou@gmail.com>
2024-07-19 10:45:17 -0500

    Make get_distro source in the current context

--- depends/setup.sh
+++ depends/setup.sh
@@ -17,38 +17,42 @@ die() {
     exit 1
 }
 
-[ $UID -eq 0 ] && die 'Please run this script without root privilege'
+if [[ $UID -eq 0 ]]; then
+    die 'Please run this script without root privilege'
+fi
 
 #
-# Output a short name of the Linux distribution
+# Output a short name (v:DISTRO) of the Linux distribution
 #
 get_distro() {
+    export DISTRO
+
     if test -f /etc/os-release; then # freedesktop.org and systemd
         source /etc/os-release
-        echo "$NAME" | cut -f 1 -d ' ' | tr '[:upper:]' '[:lower:]'
+        DISTRO="$(echo "$NAME" | cut -f 1 -d ' ' | tr '[:upper:]' '[:lower:]')"
     elif type lsb_release >/dev/null 2>&1; then # linuxbase.org
-        lsb_release -si | tr '[:upper:]' '[:lower:]'
+        DISTRO="$(lsb_release -si | tr '[:upper:]' '[:lower:]')"
     elif test -f /etc/lsb-release; then
         # shellcheck source=/dev/null
         source /etc/lsb-release
-        echo "$DISTRIB_ID" | tr '[:upper:]' '[:lower:]'
+        DISTRO="$(echo "$DISTRIB_ID" | tr '[:upper:]' '[:lower:]')"
     elif test -f /etc/arch-release; then
-        echo "arch"
+        DISTRO="arch"
     elif test -f /etc/debian_version; then
         # Older Debian, Ubuntu
-        echo "debian"
+        DISTRO="debian"
     elif test -f /etc/SuSe-release; then
         # Older SuSE
-        echo "opensuse"
+        DISTRO="opensuse"
     elif test -f /etc/fedora-release; then
         # Older Fedora
-        echo "fedora"
+        DISTRO="fedora"
     elif test -f /etc/redhat-release; then
         # Older Red Hat, CentOS
-        echo "centos"
+        DISTRO="centos"
     elif type uname >/dev/null 2>&1; then
         # Fall back to uname
-        uname -s
+        DISTRO="$(uname -s)"
     else
         die 'Unable to determine the distribution'
     fi
@@ -203,8 +207,7 @@ aur_install() {
         git clone "https://aur.archlinux.org/$TARGET.git"
     fi
 
-    DISTRO="$(get_distro)"
-    if [ "$DISTRO" = "arch" ]; then
+    if [[ "$DISTRO" == "arch" ]]; then
         (makepkg_arch "$TARGET" "$@")
     else
         (makepkg_manual "$TARGET" "$@")
@@ -213,7 +216,7 @@ aur_install() {
 }
 
 main() {
-    DISTRO="$(get_distro)"
+    get_distro
 
     if [[ "$DISTRO" == "arch" ]]; then
         if ! pacman -Q paru >/dev/null 2>&1; then
