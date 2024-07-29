c715401e80f1b6254a92bcbf81900bf98a2226ae
kyechou <kuanyenchou@gmail.com>
2024-07-29 17:49:29 -0500

    update example 17 run script

--- examples/17-campus-network/run.sh
+++ examples/17-campus-network/run.sh
@@ -4,10 +4,10 @@ SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 source "${SCRIPT_DIR}/../common.sh"
 
 # # Test
-# emu_pct=10
+# emu_pct=8
 # invs=4
-# "${CONFGEN[@]}" --data "$SCRIPT_DIR/data" -e "$emu_pct" -i "$invs" -l >"$CONF"
-# procs=8
+# "${CONFGEN[@]}" -e "$emu_pct" -i "$invs" >"$CONF"
+# procs=4
 # drop=timeout
 # name="output.$emu_pct-emulated.$invs-invariants.$procs-procs.$drop"
 # run "$name" "$procs" "$drop" "$CONF" #--parallel-invs
@@ -15,7 +15,7 @@ source "${SCRIPT_DIR}/../common.sh"
 
 for emu_pct in 4 8 12 16 20; do
     for invs in 1 4 8 12 16; do
-        "${CONFGEN[@]}" --data "$SCRIPT_DIR/data" -e "$emu_pct" -i "$invs" -l >"$CONF"
+        "${CONFGEN[@]}" --data "$SCRIPT_DIR/data" -e "$emu_pct" -i "$invs" >"$CONF"
         for procs in 1 4 8 12 16; do
             for drop in "${DROP_METHODS[@]}"; do
                 name="output.$emu_pct-emulated.$invs-invariants.$procs-procs.$drop"
