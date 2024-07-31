b702bfe6f9f94c3c08c6b2de44c93c63541fa599
kyechou <kuanyenchou@gmail.com>
2024-07-31 17:20:31 -0500

    gitignore bridges file and fix the common script path

--- full-emulation/00-reverse-path-filtering/run.sh
+++ full-emulation/00-reverse-path-filtering/run.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 EXAMPLE_DIR="$SCRIPT_DIR/../../examples/00-reverse-path-filtering"
 export TOML_INPUT="$EXAMPLE_DIR/network.fault.toml"
-source "$SCRIPT_DIR/../../examples/common.sh"
+source "$SCRIPT_DIR/..//common.sh"
 
 create_bridges
 sudo containerlab deploy -t "$CONF"
