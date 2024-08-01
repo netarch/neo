a7fba75f3d82811e45455e213d7cec5303a66ee0
kyechou <kuanyenchou@gmail.com>
2024-08-01 10:11:08 -0500

    Update format

--- scripts/parse-logs.sh
+++ scripts/parse-logs.sh
@@ -16,15 +16,21 @@ die() {
 
 [ $UID -eq 0 ] && die 'Please run this script without root privilege'
 
-targets=(
-    "$PROJECT_DIR/examples/15-real-networks"
-    "$PROJECT_DIR/examples/17-campus-network"
-    "$PROJECT_DIR/examples/18-fat-tree-datacenter"
-    "$PROJECT_DIR/full-emulation/00-reverse-path-filtering"
-    "$PROJECT_DIR/full-emulation/15-real-networks"
-    "$PROJECT_DIR/full-emulation/17-campus-network"
-)
+main() {
+    local targets=(
+        "$PROJECT_DIR/examples/15-real-networks"
+        "$PROJECT_DIR/examples/17-campus-network"
+        "$PROJECT_DIR/examples/18-fat-tree-datacenter"
+        "$PROJECT_DIR/full-emulation/00-reverse-path-filtering"
+        "$PROJECT_DIR/full-emulation/15-real-networks"
+        "$PROJECT_DIR/full-emulation/17-campus-network"
+    )
 
-for target in "${targets[@]}"; do
-    python3 "$SCRIPT_DIR/logparser.py" -t "$target"
-done
+    for target in "${targets[@]}"; do
+        python3 "$SCRIPT_DIR/logparser.py" -t "$target"
+    done
+}
+
+main "$@"
+
+# vim: set ts=4 sw=4 et:
