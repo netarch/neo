0fd70530b5e82db940dc7686fe4564c7a5948d74
kyechou <kuanyenchou@gmail.com>
2024-08-01 10:10:20 -0500

    Add root log parsing trigger

---
+++ scripts/parse-logs.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
+
+msg() {
+    echo -e "[+] ${1-}" >&2
+}
+
+die() {
+    echo -e "[!] ${1-}" >&2
+    exit 1
+}
+
+[ $UID -eq 0 ] && die 'Please run this script without root privilege'
+
+targets=(
+    "$PROJECT_DIR/examples/15-real-networks"
+    "$PROJECT_DIR/examples/17-campus-network"
+    "$PROJECT_DIR/examples/18-fat-tree-datacenter"
+    "$PROJECT_DIR/full-emulation/00-reverse-path-filtering"
+    "$PROJECT_DIR/full-emulation/15-real-networks"
+    "$PROJECT_DIR/full-emulation/17-campus-network"
+)
+
+for target in "${targets[@]}"; do
+    python3 "$SCRIPT_DIR/logparser.py" -t "$target"
+done
